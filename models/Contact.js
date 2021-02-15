60e95b96cd40ca6c731685e7e9818afe35fffb10
Омельянов Дима <domelyanov@hotmail.com>
2021-02-15 15:14:34 +0200

    c

---
+++ models/Contact.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Dima",
+    email: "adam@gmail.com",
+    phone: "099 999 44 88",
+  },
+  {
+    id: 2,
+    name: "Vova",
+    email: "Vova@gmail.com",
+    phone: "099 999 44 88",
+  },
+  {
+    id: 3,
+    name: "Jhon",
+    email: "Jhon@gmail.com",
+    phone: "099 999 44 88",
+  },
+];
